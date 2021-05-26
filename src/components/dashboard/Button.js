1ec02f275679444c3cd9df8ce3324371cac9f141
UniAccAK <alexander.kokoreff@stud.hs-bochum.de>
2021-05-26 19:56:15 +0200

    Button funkt

--- src/components/dashboard/Button.js
+++ src/components/dashboard/Button.js
@@ -4,18 +4,30 @@ import "../../css/Dashboard.css";
 class Button extends Component {
   state = {};
   render() {
+    if (this.props.menuOpen) {
     return (
       <div className="btn-container">
-        <button className="btn">
           <img
             className="logo"
             src={this.props.logoid}
             alt="Error Displaying"
           ></img>
-          <span className="btn-text">{this.props.component}</span>
-        </button>
       </div>
     );
+    } else {
+      return (
+        <div className="btn-container">
+          <button className="btn">
+            <img
+              className="logo"
+              src={this.props.logoid}
+              alt="Error Displaying"
+            ></img>
+            <span className="btn-text">{this.props.component}</span>
+          </button>
+        </div>
+      );
+    }
   }
 }
 
