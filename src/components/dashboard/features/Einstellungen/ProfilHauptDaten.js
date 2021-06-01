c3ea366a4e37c564e8ac67cde57cf2c0046f782e
UniAccAK <alexander.kokoreff@stud.hs-bochum.de>
2021-06-01 19:29:18 +0200

    Dashboard: Einstellungen -> Added Input Fields - Alexander

--- src/components/dashboard/features/Einstellungen/ProfilHauptDaten.js
+++ src/components/dashboard/features/Einstellungen/ProfilHauptDaten.js
@@ -7,6 +7,14 @@ const ProfilHauptDaten = () => {
   return (
         <div className="profile-first-container">
             <h1>Name, Vorname, Bild usw. hier</h1>
+            <div className="wrapper-for-img"></div>
+            <div className="profile-input-container">
+            <input className="vorname-field" placeholder="Vorname"></input>
+            <input className="nachname-field" placeholder="Nachname"></input>
+            <input className="strasse-field" placeholder="Strasse"></input>
+            <input className="plz-field" placeholder="Postleitzahl"></input>
+            <input className="stadt-field" placeholder="Stadt"></input>
+            </div>
             <hr id="horizontal-trenner-e" width="1" size="595"></hr>
         </div>
   );
