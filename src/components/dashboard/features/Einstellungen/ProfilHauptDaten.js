1bcc0fee65863b9c6846afef8f34d34b0360bee5
UniAccAK <alexander.kokoreff@stud.hs-bochum.de>
2021-06-01 19:41:16 +0200

    Dashboard: Einstellungen -> Added erstes Profilbild-Grundgerüst - Alexander

--- src/components/dashboard/features/Einstellungen/ProfilHauptDaten.js
+++ src/components/dashboard/features/Einstellungen/ProfilHauptDaten.js
@@ -6,8 +6,9 @@ const ProfilHauptDaten = () => {
 
   return (
         <div className="profile-first-container">
-            <h1>Name, Vorname, Bild usw. hier</h1>
-            <div className="wrapper-for-img"></div>
+            <div className="wrapper-for-img">
+              <input type="file" className="pfp-upload-img"></input>
+            </div>
             <div className="profile-input-container">
             <input className="vorname-field" placeholder="Vorname"></input>
             <input className="nachname-field" placeholder="Nachname"></input>
--- src/components/dashboard/features/Einstellungen/ProfilNebenDaten.js
+++ src/components/dashboard/features/Einstellungen/ProfilNebenDaten.js
@@ -6,7 +6,6 @@ const ProfilNebenDaten = () => {
 
   return (
         <div className="profile-second-container">
-            <h1>Password und Email ändern hier</h1>
         </div>
   );
 };
