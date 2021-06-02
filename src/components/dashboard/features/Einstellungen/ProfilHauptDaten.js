70219fa4af9fe240e54c90ecdcb17bb3e527b7c4
UniAccAK <alexander.kokoreff@stud.hs-bochum.de>
2021-06-02 19:13:33 +0200

    Dashboard: Einstellungen Profil Grunddesign - Alexander

--- src/components/dashboard/features/Einstellungen/ProfilHauptDaten.js
+++ src/components/dashboard/features/Einstellungen/ProfilHauptDaten.js
@@ -1,4 +1,5 @@
 import "./ProfilHauptDaten.css";
+import EinstellungInput from "./EinstellungInput.js";
 
 
 const ProfilHauptDaten = () => {
@@ -7,15 +8,14 @@ const ProfilHauptDaten = () => {
   return (
         <div className="profile-first-container">
             <div className="wrapper-for-img">
-              <input type="file" className="pfp-upload-img"></input>
+            <input type="file" className="pfp-upload-img"></input>
             </div>
-            <div className="profile-input-container">
-            <input className="vorname-field" placeholder="Vorname"></input>
-            <input className="nachname-field" placeholder="Nachname"></input>
-            <input className="strasse-field" placeholder="Strasse"></input>
-            <input className="plz-field" placeholder="Postleitzahl"></input>
-            <input className="stadt-field" placeholder="Stadt"></input>
-            </div>
+            <EinstellungInput text="Vorname" identifier="field-vorname-container" styling={{position: "absolute", top: "30%", left: "10%", width: "15%", height: "10%"}}/>
+            <EinstellungInput text="Nachname" identifier="field-nachname-container" styling={{position: "absolute", top: "30%", left: "50%", width: "15%", height: "10%"}}/>
+            <EinstellungInput text="Straße" identifier="field-strasse-container" styling={{position: "absolute", top: "45%", left: "10%", width: "15%", height: "10%"}}/>
+            <EinstellungInput text="Postleitzahl" identifier="field-plz-container" styling={{position: "absolute", top: "60%", left: "10%", width: "15%", height: "10%"}}/>
+            <EinstellungInput text="Stadt" identifier="field-stadt-container" styling={{position: "absolute", top: "60%", left: "50%", width: "15%", height: "10%"}}/>
+            <button id="speichern-einstellungen-btn">Speichern</button>
             <hr id="horizontal-trenner-e" width="1" size="595"></hr>
         </div>
   );
