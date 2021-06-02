d021f3572109599e61921b6ce558731f32a8dcd9
UniAccAK <alexander.kokoreff@stud.hs-bochum.de>
2021-06-02 20:11:32 +0200

    Dashboard: Einstellungen Anfang von Responsive - Alexander

--- src/components/dashboard/features/Einstellungen/ProfilHauptDaten.js
+++ src/components/dashboard/features/Einstellungen/ProfilHauptDaten.js
@@ -12,7 +12,7 @@ const ProfilHauptDaten = () => {
             </div>
             <EinstellungInput text="Vorname" identifier="field-vorname-container" styling={{position: "absolute", top: "30%", left: "0", width: "15%", height: "10%"}}/>
             <EinstellungInput text="Nachname" identifier="field-nachname-container" styling={{position: "absolute", top: "30%", left: "25%", width: "15%", height: "10%"}}/>
-            <EinstellungInput text="Straße" identifier="field-strasse-container" styling={{position: "absolute", top: "45%", left: "0", width: "15%", height: "10%"}}/>
+            <EinstellungInput text="Strasse" identifier="field-strasse-container" styling={{position: "absolute", top: "45%", left: "0", width: "15%", height: "10%"}}/>
             <EinstellungInput text="Postleitzahl" identifier="field-plz-container" styling={{position: "absolute", top: "60%", left: "0", width: "15%", height: "10%"}}/>
             <EinstellungInput text="Stadt" identifier="field-stadt-container" styling={{position: "absolute", top: "60%", left: "25%", width: "15%", height: "10%"}}/>
             <button id="speichern-einstellungen-btn">Speichern</button>
@@ -22,7 +22,7 @@ const ProfilHauptDaten = () => {
             <EinstellungInput text="Neues Passwort bestätigen" identifier="field-newpwagain-container" styling={{position: "absolute", top: "25%", left: "60%", width: "20%", height: "10%"}}/>
             <button id="pwchange-einstellungen-btn">Passwort ändern</button>
             <hr id="linebreaker-rightside-einstellungen"></hr>
-            <EinstellungInput text="Email-Adresse" identifier="field-email-container" styling={{position: "absolute", top: "60%", left: "60%", width: "15%", height: "10%"}} />
+            <EinstellungInput text="E-Mail-Adresse" identifier="field-email-container" styling={{position: "absolute", top: "60%", left: "60%", width: "15%", height: "10%"}} />
             <button id="emailchange-einstellungen-btn">E-Mail Aktualisieren</button>
         </div>
   );
