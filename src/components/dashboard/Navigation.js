c6dfb98767bdbde39e9ea6d870723a48e6c6ac7b
UniAccAK <alexander.kokoreff@stud.hs-bochum.de>
2021-06-03 21:13:52 +0200

    Dashboard: Pfeile für News hinzugefügt - Alexander

--- src/components/dashboard/Navigation.js
+++ src/components/dashboard/Navigation.js
@@ -1,6 +1,8 @@
 import "./Navigation.css";
 import profil_logo from "../../img/dashboard/Profil.svg";
 import menu_logo from "../../img/dashboard/downloads-menu.svg";
+import lpfeil_logo from "../../img/dashboard/Pfeil nach links.svg";
+import rpfeil_logo from "../../img/dashboard/Pfeil nach rechts.svg";
 /* hamburger menu momentan schlecht, da weiß auf weiß */
 
 const Navigation = ({ menuOpen, openMenu, closeMenu }) => {
@@ -13,9 +15,11 @@ const Navigation = ({ menuOpen, openMenu, closeMenu }) => {
         <img width="20" src={menu_logo} alt="|||" />
       </div>
       <div className="navigation-left">
+        <img id="change-news-left" src={lpfeil_logo} alt="lpf"/>
         <a href="/news">
           -- Das Nächste WT-2 Praktikum findet am 9.06 um 14:50 statt! --
         </a>
+        <img id="change-news-right" src={rpfeil_logo} alt="rpf"/>
       </div>
       <div className="navigation-right">
         <div className="dropdown">
