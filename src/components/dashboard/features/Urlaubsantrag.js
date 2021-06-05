acc825f5f9f6688c0b279c7b44b52e81f30be2d9
UniAccAK <alexander.kokoreff@stud.hs-bochum.de>
2021-06-05 19:35:18 +0200

    Dashboard: Urlaubsantrag, Info - Alexander

--- src/components/dashboard/features/Urlaubsantrag.js
+++ src/components/dashboard/features/Urlaubsantrag.js
@@ -1,6 +1,6 @@
 
 import "./Urlaubsantrag.css";
-
+import info_logo from "../../../img/dashboard/info-urlaubsantrag.png";
 
 // TODO: Responsive, evtl. Date time für Von-Bis Daten & "Form" für POST request
 
@@ -21,12 +21,12 @@ const Urlaubsantrag = () => {
                 <label for="sonder">Sonderurlaub</label> 
                 </div>
               {/* Evtl. type=date */}
-              <input className="von-field" placeholder="Von [TT.MM.JJJJ]"></input>
-              <input className="bis-field" placeholder="Bis [TT.MM.JJJJ]"></input>
+              <input type="date" className="von-field" placeholder="Von [TT.MM.JJJJ]"></input>
+              <input type="date" className="bis-field" placeholder="Bis [TT.MM.JJJJ]"></input>
 
               <textarea id="message-field-urlaubsantrag" placeholder="Schreiben Sie einen Kommentar..."></textarea>
               <span id="info-text-urlaubsantrag">
-              &#x1F6C8; Mit der Bestätigung des Antrag-Buttons bestätigen Sie die Richtigkeit<br />
+              <img width="20" src={info_logo} alt="info"/> Mit der Bestätigung des Antrag-Buttons bestätigen Sie die Richtigkeit<br />
                 Ihrer Angaben. Eine Änderung des vorstehend genehmigten Urlaubs<br />
                 bedarf der Zustimmung des Arbeitgebers.
               </span>
